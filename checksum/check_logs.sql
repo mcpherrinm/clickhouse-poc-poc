44292973549e859ef1158838b3d307d0fb32ecb9
Matthew McPherrin <git@mcpherrin.ca>
2025-10-01 10:34:57 -0400

    small extra tweaks to check_logs.sql
    This is good to go now

--- checksum/check_logs.sql
+++ checksum/check_logs.sql
@@ -5,17 +5,17 @@ SELECT
     LogAttributes.checksum as checksum,
     base64URLEncode(reinterpretAsFixedString(CRC32(Body))) as computed
 FROM otel.otel_logs
-WHERE checksum is null or checksum != computed
+WHERE checksum IS NULL or checksum != computed
 ORDER BY Timestamp desc
 LIMIT 1000;
 
 -- Count how many logs are missing a checksum, have a matching checksum, or mismatching, by day.
 SELECT
-    toYYYYMMDD(Timestamp) as date,
+    toDate(Timestamp) as date,
     LogAttributes.checksum IS NOT NULL as hasChecksum,
     base64URLEncode(reinterpretAsFixedString(CRC32(Body))) == LogAttributes.checksum as match,
     count(*)
 FROM otel.otel_logs
-GROUP BY hasChecksum, match, date
+GROUP BY ALL
 ORDER BY date desc
 LIMIT 1000;
